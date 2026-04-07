826c23119a1f645f0266d7c3b7b78acb8f6a25c5
marthe-risoy <marthe-e-risoy@hotmail.com>
2026-04-07 12:46:11 +0200

    Created transactions.ts

---
+++ client/src/features/transactions/types/transactions.ts
@@ -0,0 +1,14 @@
+export type Transaction = {
+  id: string;
+  customerId: string;
+  customerName: string;
+  fundName: string;
+  shareClass: string;
+  tradeDate: string;
+  settlementDate: string;
+  transactionType: "Andre kostnader" | "Kildeskatt" | "Kjøp" | "Kostnadsførte kjøpsomk." | "Relaisert valutatap (Disagio)" | "Salg" | "Tildelt ved aksjesplitt" | "Urealisert gevinst" | "Urealisert tap" | "Utbytte";
+  units: number;
+  price: number;
+  amount: number;
+  unitEffect: number;
+};
