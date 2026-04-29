97d1da54d3a748c1216996286bff2dfcbe538bf7
marthe-risoy <marthe-e-risoy@hotmail.com>
2026-04-29 13:11:08 +0200

    Changed dividend icon

--- client/src/app/navigation/navItems.tsx
+++ client/src/app/navigation/navItems.tsx
@@ -217,15 +217,8 @@ function TradeIcon() {
 
 function DividendIcon() {
   return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" className="app-icon">
-      <path
-        d="M12 5v14M8.5 8.5c0-1.7 1.5-3 3.5-3s3.5 1.3 3.5 3-1.5 3-3.5 3-3.5 1.3-3.5 3 1.5 3 3.5 3 3.5-1.3 3.5-3"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.8"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
+      <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z"/>
     </svg>
   );
 }
