7c6b6b4206e5c817b55366624900ec53d272b2ba
marthe-risoy <marthe-e-risoy@hotmail.com>
2026-04-29 13:32:30 +0200

    Fix dividend icon

--- client/src/app/navigation/navItems.tsx
+++ client/src/app/navigation/navItems.tsx
@@ -1,4 +1,4 @@
-﻿import type { ComponentType } from 'react';
+﻿﻿﻿import type { ComponentType } from 'react';
 import CompliancePage from '../../features/compliance/pages/CompliancePage';
 import CustomersPage from '../../features/customers/pages/CustomersPage';
 import DashboardPage from '../../features/dashboard/pages/DashboardPage';
@@ -217,8 +217,14 @@ function TradeIcon() {
 
 function DividendIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
-      <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z"/>
+    <svg viewBox="-4 -4 24 24" aria-hidden="true" className="app-icon">
+      <path 
+        d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="1"
+      />
     </svg>
   );
 }
